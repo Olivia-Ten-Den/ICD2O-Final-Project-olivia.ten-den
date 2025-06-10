c020cdcbeced406656e83b001f92b77782141718
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-06-10 17:12:18 +0000

    added first if statment and declared dice variables

--- js/script.js
+++ js/script.js
@@ -5,3 +5,35 @@
 // This file contains the JS functions for index.html
 
 'use strict'
+
+function rollAllDice(){
+  let numberOfDice = document.getElementById("dice")
+  let dieOne = document.getElementById("die-one")
+  let dieTwo = document.getElementById("die-two")
+  let dieThree = document.getElementById("die-three")
+  let dieFour = document.getElementById("die-four")
+  let dieFive = document.getElementById("die-five")
+  let dieSix = document.getElementById("die-six")
+
+  if (numberOfDice = 1){
+    <p>1</p>
+}
+  else if (numberOfDice = 2){
+    <p>2</p>
+  }
+  else if (numberOfDice = 3){
+    <p>3</p>
+  }
+  else if (numberOfDice = 4){
+    <p>4</p>
+  }
+  else if (numberOfDice = 5){
+    <p>5</p>
+  }
+  else if (numberOfDice = 6){
+    <p>6</p>
+  }
+  else{
+    <p>Something went wrong.</p>
+  }
+}
